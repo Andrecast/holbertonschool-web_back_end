7ee51d4e780cb17916b4510371252dc0b1a2b34d
Andrecast <acastrillonpuerta@gmail.com>
2022-01-31 21:52:23 -0500

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+        Return:
+            Float number truncated to floor
+    """
+
+    return int(n)
