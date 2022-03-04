64ff9b97a05cf78724173ac7a44129d474baa30d
Andrecast <acastrillonpuerta@gmail.com>
2022-03-04 17:07:27 -0500

    4-app.py

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python3
-"""
-App module
-"""
-from flask import Flask, request, render_template
-from flask_babel import Babel
-from typing import Union
-
+""" Basic Flask app, Basic Babel setup, Get locale from request,
+    Parametrize templates, Force locale with URL parameter """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 babel = Babel(app)
+""" instantiate the Babel object """
 
 
 class Config(object):
-    """ Class config app
-    """
+    """ config class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale() -> Union[str, None]:
-    """ get locale
-    """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+""" Use that class as config for Flask app """
 
 
 @app.route('/')
-def index() -> str:
-    """ index
-    """
-    return render_template('4-index.html')
+def root():
+    """ basic Flask app """
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ to determine the best match with our supported languages """
+    localLang = request.args.get('locale')
+    supportLang = app.config['LANGUAGES']
+    if localLang in supportLang:
+        return localLang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
+
