ae5cc262d766f1f7e6277ea2a0fe7949c9b550db
andrea <acastrillon@ec.krugercorp.com>
2022-04-11 22:21:39 -0500

    tasks 7 and 8

---
+++ 0x12-Node_JS_basic/full_server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const port = 1245;
+
+controllerRouting(app);
+
+app.listen(port, () => { });
+
+export default app;
